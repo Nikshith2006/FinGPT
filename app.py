ad23406e2bcbf9a0008b211bad745beab39a5c1e
Nikshith <nikshithdoppa@gmail.com>
2026-03-06 11:48:16 +0530

    Refactor authentication handling in app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
-from features.auth import login
+from features.auth import login_page
 from features.dashboard import dashboard
 
 st.set_page_config(page_title="FinGPT", layout="wide")
 
-if "user" not in st.session_state:
-    st.session_state.user = None
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
 
-if st.session_state.user is None:
-    login()
-else:
+if st.session_state.logged_in:
     dashboard()
+else:
+    login_page()
