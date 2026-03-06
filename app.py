4b7bfa2e68c60036a18b004ee62b9b07aeb0c626
Nikshith <nikshithdoppa@gmail.com>
2026-03-06 12:29:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from dotenv import load_dotenv
-import os
 
 from features.auth import login
 from features.dashboard import dashboard
@@ -9,14 +8,12 @@ st.set_page_config(page_title="FinGPT", layout="wide")
 
 load_dotenv()
 
-# Session setup
 if "user" not in st.session_state:
     st.session_state.user = None
 
 if "edit_mode" not in st.session_state:
     st.session_state.edit_mode = False
 
-# Routing
 if st.session_state.user is None:
     login()
 else:
