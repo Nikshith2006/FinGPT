096d0ba04134b8aa3071b78d5c9ebe89b189b855
Nikshith <nikshithdoppa@gmail.com>
2026-03-06 23:50:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from dotenv import load_dotenv
 from features.auth import login
 from features.dashboard import dashboard
 
+if "voice_text" not in st.session_state:
+st.session_state.voice_text = ""
+
+
 st.set_page_config(page_title="FinGPT", layout="wide")
 
 load_dotenv()
@@ -18,3 +22,4 @@ if st.session_state.user is None:
     login()
 else:
     dashboard()
+
