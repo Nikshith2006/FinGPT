4e98771379f9b2ed81b4c9bf660749150355db8f
Nikshith <nikshithdoppa@gmail.com>
2026-03-10 00:12:38 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,19 +2,24 @@ import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 
+# Google API scopes
 scope = [
-"https://www.googleapis.com/auth/spreadsheets",
-"https://www.googleapis.com/auth/drive"
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
 ]
 
+# Load credentials from Streamlit Secrets
 creds = Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
     scopes=scope
 )
 
+# Authorize client
 client = gspread.authorize(creds)
 
-sheet = client.open("FinGPT Database")
+# Open your Google Sheet
+spreadsheet = client.open("FinGPT Database")
 
-expenses_sheet = sheet.worksheet("expenses")
-users_sheet = sheet.worksheet("users")
+# Access worksheets
+expenses_sheet = spreadsheet.worksheet("expenses")
+users_sheet = spreadsheet.worksheet("users")
